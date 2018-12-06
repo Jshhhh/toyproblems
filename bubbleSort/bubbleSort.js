03de1578aa05831158035746dc945a1060e49ac6
Jshhhh <36183898+Jshhhh@users.noreply.github.com>
2018-12-06 12:18:51 -0800

    Implement bubleSort

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -1,58 +1,26 @@
-/*jshint expr:true*/
-
-/*
- * Bubble sort is the most basic sorting algorithm in all of Computer
- * Sciencedom. It works by starting at the first element of an array and
- * comparing it to the second element; if the first element is greater than the
- * second element, it swaps the two. It then compares the second to the third,
- * and the third to the fourth, and so on; in this way, the largest values
- * "bubble" to the end of the array. Once it gets to the end of the array, it
- * starts over and repeats the process until the array is sorted numerically.
- *
- * Implement a function that takes an array and sorts it using this technique.
- * Don't use JavaScript's built-in sorting function (Array.prototype.sort).
- *
- * QUERY: What's the time complexity of your algorithm? If you don't already
- * know, try to intuit this without consulting the Googles.
- *
- * Extra credit: Optimization time! During any given pass, if no elements are
- * swapped we can assume the list is sorted and can exit the function early.
- * After this optimization, what is the time complexity of your algorithm?
- *
- * Moar credits: Do you need to consider every element every time you iterate
- * through the array? Make it happen, boss. Again: Has the time complexity of
- * your algorithm changed?
-*/
-
-/*
- * Example usage:
- * bubbleSort([2, 1, 3]); // yields [1, 2, 3]
- *
-*/
-
-// Feel free to add helper functions if needed.
-
 
 var bubbleSort = function(array) {
   let sortedArray = array;
-  // compare first element and second element
-    // if first element is greater than second element
-      //swap first element with second element
-  function sort(i) {
-      let current = sortedArray[i];
-      let next = sortedArray[i + 1];
-      let sorted = false
-      if (current > next) {
-        sortedArray[i] = next;
-        sortedArray[i + 1] = current;
-        sorted = true;
+  let current = 0;
+  let index = 1;
+  let sorting = true;
+
+  while (sorting) {
+    sorting = false;
+    for (let i = 0; i < array.length; i++) {
+      let curr = array[i];
+      for (let j = i+1; j < array.length; j++) {
+        let next = array[j];
+        if (curr > next) {
+          sorting = true;
+          array[i] = next;
+          array[j] = curr;
+          i = j;
+        }
       }
-      if (sorted) {
-        sort(0);
-      }
+    }
   }
-  sort(0);
-  return sortedArray;
+    return array;
 };
 
-console.log(bubbleSort([3, 2, 1, 0]));
+console.log(bubbleSort([2,1]));
